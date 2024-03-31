86c38a59c2986845f02371e77a9c0141688d0e5f
Zhu Mingfeng <zmaibbs7@163.com>
2024-03-31 09:48:57 +0800

    add: videoPlayer - 支持解码格式输出

--- QtQuick/videoPlayer/main.cpp
+++ QtQuick/videoPlayer/main.cpp
@@ -1,5 +1,32 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QtMultimedia/QMediaPlayer>
+#include <QtMultimedia/QMediaService>
+
+#include <QMediaRecorder>
+#include <QAudioRecorder>
+#include <QDebug>
+#include <QStringList>
+
+void printSupportedMediaTypes() {
+    QAudioRecorder recorder;
+
+    // 获取支持的音频编解码器
+    QStringList audioCodecs = recorder.supportedAudioCodecs();
+    qDebug() << "Supported audio codecs:";
+    for (const QString &codecName : audioCodecs) {
+        QString description = recorder.audioCodecDescription(codecName);
+        qDebug() << codecName << ":" << description;
+    }
+
+    // 获取支持的容器格式
+    QStringList containers = recorder.supportedContainers();
+    qDebug() << "\nSupported containers:";
+    for (const QString &containerName : containers) {
+        QString description = recorder.containerDescription(containerName);
+        qDebug() << containerName << ":" << description;
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -7,6 +34,9 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
     QGuiApplication app(argc, argv);
+
+    printSupportedMediaTypes();
+
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
