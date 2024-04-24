cad9e6a63fe80530681fc3d9383f46f4f4a465e7
Zhu Mingfeng <zmaibbs7@163.com>
2024-04-24 13:45:06 +0800

    云平台获取时间

--- Net/httpClient.cpp
+++ Net/httpClient.cpp
@@ -2,7 +2,9 @@
 #include "hv/requests.h"
 #include "nlohmann/json.hpp"
 
-int main() {
+//打电话
+void demo1()
+{
     using json = nlohmann::json;
 
     // 构造 JSON header
@@ -25,6 +27,63 @@ int main() {
         std::cout << resp->status_code << " " << resp->status_message() << "\r\n";
         std::cout << resp->body << "\n";
     }
+}
 
+
+//获取token
+std::string get_cloud_token()
+{
+    using json = nlohmann::json;
+
+    // 构造 JSON header
+    json body = {
+        {"secretKey", "893d1856723bfb71"},
+        {"sn", "G2403202404230001"}
+    };
+    std::string body_str = body.dump();
+
+    http_headers headers;
+    headers["Content-Type"] = "application/json";
+    auto resp = requests::post("http://47.100.126.90:443/delibot/api/auth/token", body_str, headers);
+    if (resp == nullptr) {
+        std::cout << "Request failed!\n";
+    } else 
+    {
+        std::cout << resp->status_code << " " << resp->status_message() << "\r\n";
+        std::cout << resp->body << "\n";
+    }
+
+    json response = json::parse(resp->body.c_str());
+    std::string token = response["data"]["accessToken"];
+    return token;
+}
+
+//获取最新时间
+void get_cloud_time(const std::string& toeken)
+{
+    using json = nlohmann::json;
+
+
+    http_headers headers;
+    headers["Content-Type"] = "application/x-www-form-urlencoded";
+    headers["accessToken"] = toeken;
+
+    // 发送 POST 请求
+    auto resp = requests::get("http://47.100.126.90:443/delibot/api/device/info", headers);
+    if (resp == nullptr) {
+        std::cout << "Request failed!\n";
+    } else 
+    {
+        std::cout << resp->status_code << " " << resp->status_message() << "\r\n";
+        std::cout << resp->body << "\n";
+    }
+}
+
+int main() {
+
+
+    std::string token = get_cloud_token();
+    std::cout << "token = " << token << std::endl;
+    get_cloud_time(token);
     return 0;
 }
