9fa8f3f8e58ae63c5ed0fc25d954d5a5a1c3c2d7
Zhu Mingfeng <zmaibbs7@163.com>
2024-05-16 23:50:31 +0800

    升级breakPad并增加了Windows的mini_walk

--- qBreakPad/modules/qBreakpad/third_party/breakpad/src/common/linux/dump_symbols.cc
+++ qBreakPad/modules/qBreakpad/third_party/breakpad/src/common/linux/dump_symbols.cc
@@ -315,31 +315,17 @@ uint32_t GetCompressionHeader(
 
 std::pair<uint8_t *, uint64_t> UncompressZlibSectionContents(
     const uint8_t* compressed_buffer, uint64_t compressed_size, uint64_t uncompressed_size) {
-  z_stream stream;
-  memset(&stream, 0, sizeof stream);
-
-  stream.avail_in = compressed_size;
-  stream.avail_out = uncompressed_size;
-  stream.next_in = const_cast<uint8_t *>(compressed_buffer);
-
   google_breakpad::scoped_array<uint8_t> uncompressed_buffer(
     new uint8_t[uncompressed_size]);
 
-  int status = inflateInit(&stream);
-  while (stream.avail_in != 0 && status == Z_OK) {
-    stream.next_out =
-      uncompressed_buffer.get() + uncompressed_size - stream.avail_out;
+  uLongf size = static_cast<uLongf>(uncompressed_size);
 
-    if ((status = inflate(&stream, Z_FINISH)) != Z_STREAM_END) {
-      break;
-    }
+  int status = uncompress(
+    uncompressed_buffer.get(), &size, compressed_buffer, compressed_size);
 
-    status = inflateReset(&stream);
-  }
-
-  return inflateEnd(&stream) != Z_OK || status != Z_OK || stream.avail_out != 0
-    ? std::make_pair(nullptr, 0)
-    : std::make_pair(uncompressed_buffer.release(), uncompressed_size);
+  return status != Z_OK
+      ? std::make_pair(nullptr, 0)
+      : std::make_pair(uncompressed_buffer.release(), uncompressed_size);
 }
 
 #ifdef HAVE_LIBZSTD
@@ -1162,6 +1148,7 @@ template<typename ElfClass>
 bool InitModuleForElfClass(const typename ElfClass::Ehdr* elf_header,
                            const string& obj_filename,
                            const string& obj_os,
+                           const string& module_id,
                            scoped_ptr<Module>& module,
                            bool enable_multiple_field) {
   PageAllocator allocator;
@@ -1185,10 +1172,14 @@ bool InitModuleForElfClass(const typename ElfClass::Ehdr* elf_header,
                          ? name_buf
                          : google_breakpad::BaseName(obj_filename);
 
-  // Add an extra "0" at the end.  PDB files on Windows have an 'age'
-  // number appended to the end of the file identifier; this isn't
-  // really used or necessary on other platforms, but be consistent.
-  string id = FileID::ConvertIdentifierToUUIDString(identifier) + "0";
+  // Use the provided module_id
+  string id = module_id.empty()
+    // Add an extra "0" at the end.  PDB files on Windows have an 'age'
+    // number appended to the end of the file identifier; this isn't
+    // really used or necessary on other platforms, but be consistent.
+    ? FileID::ConvertIdentifierToUUIDString(identifier) + "0"
+    : module_id;
+
   // This is just the raw Build ID in hex.
   string code_id = FileID::ConvertIdentifierToString(identifier);
 
@@ -1202,6 +1193,7 @@ template<typename ElfClass>
 bool ReadSymbolDataElfClass(const typename ElfClass::Ehdr* elf_header,
                             const string& obj_filename,
                             const string& obj_os,
+                            const string& module_id,
                             const std::vector<string>& debug_dirs,
                             const DumpOptions& options,
                             Module** out_module) {
@@ -1210,8 +1202,8 @@ bool ReadSymbolDataElfClass(const typename ElfClass::Ehdr* elf_header,
   *out_module = NULL;
 
   scoped_ptr<Module> module;
-  if (!InitModuleForElfClass<ElfClass>(elf_header, obj_filename, obj_os, module,
-                                       options.enable_multiple_field)) {
+  if (!InitModuleForElfClass<ElfClass>(elf_header, obj_filename, obj_os, module_id,
+                                      module, options.enable_multiple_field)) {
     return false;
   }
 
@@ -1260,6 +1252,7 @@ namespace google_breakpad {
 bool ReadSymbolDataInternal(const uint8_t* obj_file,
                             const string& obj_filename,
                             const string& obj_os,
+                            const string& module_id,
                             const std::vector<string>& debug_dirs,
                             const DumpOptions& options,
                             Module** module) {
@@ -1272,12 +1265,12 @@ bool ReadSymbolDataInternal(const uint8_t* obj_file,
   if (elfclass == ELFCLASS32) {
     return ReadSymbolDataElfClass<ElfClass32>(
         reinterpret_cast<const Elf32_Ehdr*>(obj_file), obj_filename, obj_os,
-        debug_dirs, options, module);
+        module_id, debug_dirs, options, module);
   }
   if (elfclass == ELFCLASS64) {
     return ReadSymbolDataElfClass<ElfClass64>(
         reinterpret_cast<const Elf64_Ehdr*>(obj_file), obj_filename, obj_os,
-        debug_dirs, options, module);
+        module_id, debug_dirs, options, module);
   }
 
   return false;
@@ -1286,15 +1279,16 @@ bool ReadSymbolDataInternal(const uint8_t* obj_file,
 bool WriteSymbolFile(const string& load_path,
                      const string& obj_file,
                      const string& obj_os,
+                     const string& module_id,
                      const std::vector<string>& debug_dirs,
                      const DumpOptions& options,
                      std::ostream& sym_stream) {
   Module* module;
-  if (!ReadSymbolData(load_path, obj_file, obj_os, debug_dirs, options,
+  if (!ReadSymbolData(load_path, obj_file, obj_os, module_id, debug_dirs, options,
                       &module))
     return false;
 
-  bool result = module->Write(sym_stream, options.symbol_data);
+  bool result = module->Write(sym_stream, options.symbol_data, options.preserve_load_address);
   delete module;
   return result;
 }
@@ -1305,6 +1299,7 @@ bool WriteSymbolFile(const string& load_path,
 bool WriteSymbolFileHeader(const string& load_path,
                            const string& obj_file,
                            const string& obj_os,
+                           const string& module_id,
                            std::ostream& sym_stream) {
   MmapWrapper map_wrapper;
   void* elf_header = NULL;
@@ -1323,14 +1318,14 @@ bool WriteSymbolFileHeader(const string& load_path,
   if (elfclass == ELFCLASS32) {
     if (!InitModuleForElfClass<ElfClass32>(
         reinterpret_cast<const Elf32_Ehdr*>(elf_header), obj_file, obj_os,
-        module, /*enable_multiple_field=*/false)) {
+        module_id, module, /*enable_multiple_field=*/false)) {
       fprintf(stderr, "Failed to load ELF module: %s\n", obj_file.c_str());
       return false;
     }
   } else if (elfclass == ELFCLASS64) {
     if (!InitModuleForElfClass<ElfClass64>(
         reinterpret_cast<const Elf64_Ehdr*>(elf_header), obj_file, obj_os,
-        module, /*enable_multiple_field=*/false)) {
+        module_id, module, /*enable_multiple_field=*/false)) {
       fprintf(stderr, "Failed to load ELF module: %s\n", obj_file.c_str());
       return false;
     }
@@ -1345,6 +1340,7 @@ bool WriteSymbolFileHeader(const string& load_path,
 bool ReadSymbolData(const string& load_path,
                     const string& obj_file,
                     const string& obj_os,
+                    const string& module_id,
                     const std::vector<string>& debug_dirs,
                     const DumpOptions& options,
                     Module** module) {
@@ -1354,7 +1350,7 @@ bool ReadSymbolData(const string& load_path,
     return false;
 
   return ReadSymbolDataInternal(reinterpret_cast<uint8_t*>(elf_header),
-                                obj_file, obj_os, debug_dirs, options, module);
+                                obj_file, obj_os, module_id, debug_dirs, options, module);
 }
 
 }  // namespace google_breakpad
